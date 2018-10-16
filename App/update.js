c166b4dbe1a75b9db949965796419f979549def5
Shyaam Ramesh <39254780+pixelfizz@users.noreply.github.com>
2018-10-16 20:46:56 +0530

    added more example students + clear

--- App/update.js
+++ App/update.js
@@ -1,3 +1,4 @@
 var storage = window.localStorage;
 document.getElementById("shyaam").value = storage.getItem("shyaam")
 document.getElementById("krishna").value = storage.getItem("krishna")
+document.getElementById("suriya").value = storage.getItem("suriya")
