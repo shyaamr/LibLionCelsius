d23a7ae054be63be624d01e65fa7a00abc2d8dc0
Shyaam Ramesh <39254780+pixelfizz@users.noreply.github.com>
2018-10-17 13:18:02 +0530

    changes

--- Objectz/JS/main.js
+++ Objectz/JS/main.js
@@ -1,8 +1,5 @@
 var storage = window.localStorage;
 
-function addStudent() {
-  var element = document.getElementById(idName);
-  storage.setItem(idName, element.value)
-  console.log(storage.getItem(idName))
-  dateChange()
-  }
+function addTicket() {
+    
+}
